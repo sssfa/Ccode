1236acc1d0f09a9266ace770f45c886965645635
yangkaixin <yangkaixin@byosoft.com.cn>
2023-05-04 17:49:49 +0800

    oo

---
+++ DBcode/shell.c
@@ -0,0 +1,59 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+        char *buffer;
+        size_t buffer_length;
+        size_t input_length;
+} InputBuffer;
+
+InputBuffer *new_input_buffer()
+{
+        InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+        input_buffer->buffer = NULL;
+        input_buffer->buffer_length = 0;
+        input_buffer->input_length = 0;
+
+        return input_buffer;
+}
+
+void print_prompt() { printf("ydb >> "); }
+
+void read_input(InputBuffer *input_buffer)
+{
+        size_t bytes_read =
+            getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+        if (bytes_read <= 0)
+        {
+                printf("Error reading input\n");
+                exit(EXIT_FAILURE);
+        }
+
+        // Ignore trailing newline
+        input_buffer->input_length = bytes_read - 1;
+        input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+int main(int argc, char *argv[])
+{
+        InputBuffer *input_buffer = new_input_buffer();
+        while (true)
+        {
+                print_prompt();
+                read_input(input_buffer);
+
+                if (strcmp(input_buffer->buffer, ".exit") == 0)
+                {
+                        free(input_buffer->buffer);
+                        free(input_buffer);
+                        exit(EXIT_SUCCESS);
+                }
+                else
+                        printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+
+        }
+}
